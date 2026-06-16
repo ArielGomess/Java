11e109377ab833f40e9b8641748c92c296e07671
Ariel Gomes <ariel62169886@alunos.sc.senac.br>
2026-06-15 21:10:48 -0300

    Um é igual a um

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,7 +5,9 @@ public class HelloWorld {
 
 
     public static void main(String[] args) {
-        System.out.println("Ola Mundo");
+        if(1==1){
+            System.out.println("Um é igual a um");
+        }
     }
     
 }
